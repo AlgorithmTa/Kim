2c1c0950a4f62413f130b021b7a0ea5dcb3e0124
YEJIN <yejon9858@gmail.com>
2024-01-21 12:49:34 +0900

    Update : wee2 CodingTestStudy.java

--- week2/CodingTestStudy.java
+++ week2/CodingTestStudy.java
@@ -1,25 +1,25 @@
 class CodingTestStudy {
-    private int maxAlP = 0, maxCP = 0;
+    private int maxAlP = 0, maxCoP = 0;
     private int[][] d;
     public int solution(int alp, int cop, int[][] problems) {
         setNeeds(problems);
-        d = new int[maxAlP+1][maxCP+1];
+        d = new int[maxAlP+1][maxCoP+1];
         dInit();
         alp = Math.min(alp, maxAlP);
-        cop = Math.min(cop, maxCP);
+        cop = Math.min(cop, maxCoP);
         
         
         d[alp][cop] = 0;
         
         for(int i = alp ; i <= maxAlP ; i ++){
-            for(int j = cop ; j <= maxCP ; j++){
+            for(int j = cop ; j <= maxCoP ; j++){
                 if(i < maxAlP) d[i+1][j] = Math.min(d[i+1][j], d[i][j] + 1);
-                if(j < maxCP) d[i][j+1] = Math.min(d[i][j+1], d[i][j] + 1);
+                if(j < maxCoP) d[i][j+1] = Math.min(d[i][j+1], d[i][j] + 1);
                 for(int[] problem : problems){
                     if(problem[0] <= i && problem[1] <= j){
 
                         int newAlP = Math.min(i + problem[2], maxAlP);
-                        int newCP = Math.min(j + problem[3], maxCP);
+                        int newCP = Math.min(j + problem[3], maxCoP);
 
                         d[newAlP][newCP] = Math.min(d[newAlP][newCP], d[i][j] + problem[4]);
                     }
@@ -27,13 +27,13 @@ class CodingTestStudy {
             }
         }
         
-        return d[maxAlP][maxCP];
+        return d[maxAlP][maxCoP];
     }
     
     public void setNeeds(int[][] problems){
         for(int[] problem : problems){
             maxAlP = Math.max(problem[0] , maxAlP);
-            maxCP = Math.max(problem[1] , maxCP);
+            maxCoP = Math.max(problem[1] , maxCoP);
         }
     }
     
